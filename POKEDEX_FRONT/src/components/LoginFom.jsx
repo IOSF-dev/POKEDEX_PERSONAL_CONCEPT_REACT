ccd5f7edd1bd18866ce6a9a5f6281d8756e448dd
IOSF <om2figueroa@gmail.com>
2026-04-17 00:02:57 +0200

    inputs login

--- POKEDEX_FRONT/src/components/LoginFom.jsx
+++ POKEDEX_FRONT/src/components/LoginFom.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LoginFom = () => {
   return (
-    <div className='login_DIV'></div>
+    <div className='login_DIV'>
+      <div className='login_box'>
+        <input type="text" />
+        <input type="text" />
+
+      </div>
+
+    </div>
   )
 }
 
