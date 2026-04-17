b1388de2bed44396b0e6a5d24dbcf6e0c34ac32c
IOSF <om2figueroa@gmail.com>
2026-04-17 16:01:24 +0200

    Div order inside left

--- POKEDEX_FRONT/src/pages/AdminPanel.jsx
+++ POKEDEX_FRONT/src/pages/AdminPanel.jsx
@@ -7,8 +7,25 @@ const AdminPanel = () => {
     <main className='main'>
 <HeaderComponent/>
 <div className='main_PANEL'>
-  <div className='left' ></div>
-<div className='rigth'></div>
+
+  <div className='left' >
+<div className='item_top_L'>
+  <div className='NUMBERID'></div>
+</div>
+<div className='itemMAIN'>
+  <div className='GRID'></div>
+</div>
+<div className='itemDOWN'>
+  <div className='down_Box'></div>
+  <div className='down_Box'></div>
+</div>
+  </div>
+
+<div className='rigth'>
+<div className='item1'></div>
+<div className='item1'></div>
+
+</div>
 </div>
 
     </main>
